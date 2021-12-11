f4b4b4ed7ed919675de063e37d69fbbe399f3b63
Alex Rodriguez <contact@alexrodriguez.io>
2021-12-11 00:29:04 -0800

    feat(App.js): added v6 router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
 import { useState, useEffect } from "react"
+
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -70,14 +74,23 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      { 
-        tasks.length > 0 ? 
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Task To Show'
-      }
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+        <Routes>
+          <Route path="/" element={
+          <>
+            {showAddTask && <AddTask onAdd={addTask}/>}
+            { 
+              tasks.length > 0 ? 
+              <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Task To Show'
+            }
+          </>} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
